e2c45f5abb65502c4d1be455273eb28720a85dd3
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2026-04-16 14:32:17 +0100

    DTSPO-30179 - Grant new Jenkins MIs access to keyvault

--- key-vault.tf
+++ key-vault.tf
@@ -7,9 +7,10 @@ module "ccpay-vault" {
   object_id           = var.jenkins_AAD_objectId
   resource_group_name = azurerm_resource_group.rg.name
   # group id of dcd_reform_dev_azure
-  product_group_name      = "dcd_group_fees&pay_v2"
-  common_tags             = var.common_tags
+  product_group_name = "dcd_group_fees&pay_v2"
+  common_tags        = var.common_tags
   create_managed_identity = true
+  jenkins_object_id  = data.azurerm_user_assigned_identity.jenkins.principal_id
 }
 
 module "feesregister-vault" {
@@ -25,6 +26,12 @@ module "feesregister-vault" {
   common_tags                 = var.common_tags
   managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.ccpay-shared-identity.principal_id}"]
 }
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 
 data "azurerm_key_vault" "ccpay_key_vault" {
   name                = module.ccpay-vault.key_vault_name
