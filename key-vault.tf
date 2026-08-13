938b765fdbe649f5c9008128c2ac1c223302e975
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2026-08-13 16:00:05 +0000

    Updating Terraform Formatting

--- key-vault.tf
+++ key-vault.tf
@@ -1,11 +1,11 @@
 module "ccpay-vault" {
-  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
-  name                = join("-", [var.product, var.env])
-  product             = var.product
-  env                 = var.env
-  tenant_id           = var.tenant_id
-  object_id           = var.jenkins_AAD_objectId
-  resource_group_name = azurerm_resource_group.rg.name
+  source                       = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  name                         = join("-", [var.product, var.env])
+  product                      = var.product
+  env                          = var.env
+  tenant_id                    = var.tenant_id
+  object_id                    = var.jenkins_AAD_objectId
+  resource_group_name          = azurerm_resource_group.rg.name
   grant_preview_jenkins_access = var.env == "aat"
   # group id of dcd_reform_dev_azure
   product_group_name      = "dcd_group_fees&pay_v2"
@@ -15,13 +15,13 @@ module "ccpay-vault" {
 }
 
 module "feesregister-vault" {
-  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
-  name                = join("-", [var.fr_product, var.env])
-  product             = var.fr_product
-  env                 = var.env
-  tenant_id           = var.tenant_id
-  object_id           = var.jenkins_AAD_objectId
-  resource_group_name = azurerm_resource_group.rg.name
+  source                       = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  name                         = join("-", [var.fr_product, var.env])
+  product                      = var.fr_product
+  env                          = var.env
+  tenant_id                    = var.tenant_id
+  object_id                    = var.jenkins_AAD_objectId
+  resource_group_name          = azurerm_resource_group.rg.name
   grant_preview_jenkins_access = var.env == "aat"
   # group id of dcd_reform_dev_azure
   product_group_name          = "dcd_group_fees&pay_v2"
